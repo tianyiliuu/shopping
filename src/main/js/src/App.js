7ba7e83805f3e479bb2fec4ae9f841f2c0f2f321
Tianyi Liu <tianyihenry@gmail.com>
2021-06-27 17:34:06 -0400

    release 1.0, section 10

--- src/main/js/src/App.js
+++ src/main/js/src/App.js
@@ -1,26 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Image, Table} from "react-bootstrap";
+import {useEffect, useState} from "react";
+import placeholder from './images/placeholder.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [products, setProducts] = useState([]);
+
+    const getProductsHander = () => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("api/products", requestOptions)
+            .then(response => response.json())
+            .then(result => setProducts(result._embedded.products))
+            .catch(error => console.log('error', error));
+    }
+
+    useEffect(getProductsHander, []);
+
+    const productRows = products.map(product => (
+        <tr key={product._links.self.href}>
+            <td align="center"><Image src={placeholder} height="100px" width="100px"  /></td>
+            <td>{product.name}</td>
+            <td>$ {product.unitPrice}</td>
+            <td>{product.unitsInStock}</td>
+        </tr>
+    ));
+
+    return (
+        <Container>
+            <h2>Products</h2>
+            <Table striped bordered hover size="sm">
+                <thead>
+                <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Units in Stock</th>
+                </tr>
+                </thead>
+                <tbody>
+                {productRows}
+                </tbody>
+            </Table>
+        </Container>
+    );
 }
 
 export default App;
