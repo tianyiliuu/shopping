da0d115201c184d1baeb172b4551b5479b9a2dcf
Tianyi Liu <tianyihenry@gmail.com>
2021-07-03 17:30:51 -0400

    only expose product & productCategory

--- src/main/java/com/example/shopping/repository/ProductCategoryRepository.java
+++ src/main/java/com/example/shopping/repository/ProductCategoryRepository.java
@@ -2,7 +2,9 @@ package com.example.shopping.repository;
 
 import com.example.shopping.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 
