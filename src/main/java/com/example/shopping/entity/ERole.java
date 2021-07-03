50b4cd23063fa390b3f18bbd6810aad2e973471e
Tianyi Liu <tianyihenry@gmail.com>
2021-07-02 23:11:55 -0400

    add entity (User, Role, EnumRole)

---
+++ src/main/java/com/example/shopping/entity/ERole.java
@@ -0,0 +1,7 @@
+package com.example.shopping.entity;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/example/shopping/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.shopping.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<Role> roles = new HashSet<>();
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
